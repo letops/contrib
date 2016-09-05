0e27273659aff46141b01e316f488428dc407145
Daniel <daniel.ortiz@grvtylabs.com>
2016-09-05 12:54:05 -0500

    Improved the default messages

--- contrib/django/messages.py
+++ contrib/django/messages.py
@@ -1,44 +1,64 @@
 from django.contrib import messages
-from django.utils.translation import ugettext as _ug
+from django.utils.translation import ugettext_lazy as _ugl
 
+
+GRAY = 'gray'
 GREEN = 'green'
 BLUE = 'blue'
 YELLOW = 'yellow'
 RED = 'red'
 
-SUCCESS = _ug('Success')
-ERROR = _ug('Error')
-WARNING = _ug('We are sorry')
+SUCCESS = _ugl('Success')
+ERROR = _ugl('Error')
+WARNING = _ugl('We are sorry')
 
 errors_list = {
     'title': {
-        '400': _ug('Bad Request'),
-        '401': _ug('Unauthorized'),
-        '403': _ug('Forbidden'),
-        '404': _ug('Not Found'),
-        '405': _ug('Method not allowed'),
-        '406': _ug('Not Acceptable'),
-        '409': _ug('Conflict'),
-        '415': _ug('Unsupported Media Type'),
-        '500': _ug('Internal Server Error'),
-        '503': _ug('Service Unavailable'),
+        '400': _ugl('Bad Request'),
+        '401': _ugl('Unauthorized'),
+        '403': _ugl('Forbidden'),
+        '404': _ugl('Not Found'),
+        '405': _ugl('Method not allowed'),
+        '406': _ugl('Not Acceptable'),
+        '409': _ugl('Conflict'),
+        '415': _ugl('Unsupported Media Type'),
+        '500': _ugl('Internal Server Error'),
+        '503': _ugl('Service Unavailable'),
+        'sorry': 'Lo sentimos',
     },
     'body': {
-        'no_action': _ug('We are sorry. You are trying to execute an unknown or prohibited action.'),
-        'incorrect_method': _ug('We are sorry. You are trying to execute an action with an incorrect header or method.'),
-        'missing_step': _ug('We are sorry. You must complete the previous steps to execute this action.'),
-        'bad_login': _ug('The user/password combination is invalid. Please try again.'),
-        'no_perm': _ug('You do not have enough permissions to execute this action. If this is a mistake, '
-                       'please contact the administrator.'),
-        'ticket': _ug('An internal error has occurred. The administrator has been notified.'),
-        'encryption': 'Ha ocurrido un error interno. La combinación de cer, key y contraseña es incorrecta.',
+        'no_action': _ugl('We are sorry. You are trying to execute an unknown'
+                          ' or prohibited action.'),
+        'incorrect_method': _ugl('We are sorry. You are trying to execute an'
+                                 ' action with an incorrect header or'
+                                 ' method.'),
+        'missing_step': _ugl('We are sorry. You must complete the previous'
+                             ' steps to execute this action.'),
+        'bad_login': _ugl('The user/password combination is invalid. Please'
+                          ' try again.'),
+        'no_perm': _ugl('You do not have enough permissions to execute this'
+                        ' action. If this is a mistake, please contact the'
+                        ' administrator.'),
+        'ticket': _ugl('An internal error has occurred. The administrator has'
+                       ' been notified.'),
+        'encryption': 'La combinación de cer, key y contraseña es incorrecta.'
+                      ' Los cambios en estos 3 campos se han descartado.',
+        'verification_pending': 'Aún no ha verificado su correo electrónico.',
+        'verification_error': 'El correo no existe en el sistema o ya fue'
+                              ' verificado anteriormente.',
+        'not_in_team': 'Usted no pertenece a este negocio.',
+        'no_team': 'El negocio no existe en el sistema.',
+        'already_claimed': 'Esta invitación ya fue procesada anteriormente.',
+        'already_in_team': 'El usuario ya se encuentra en este negocio.',
     }
 }
 
 
-def generate_msg(request, state=BLUE, title='Empty', body='Empty'):
+def generate_msg(request, state=GRAY, title='Debug', body='Empty'):
     mtype = None
-    if state == GREEN:
+    if state == GRAY:
+        mtype = messages.DEBUG
+    elif state == GREEN:
         mtype = messages.SUCCESS
     elif state == BLUE:
         mtype = messages.INFO
