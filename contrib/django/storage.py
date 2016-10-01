8403a798a51382c4df3236a8db94b162615788e8
Daniel <daniel.ortizp91@gmail.com>
2016-09-30 19:19:03 -0500

    DJango storage, overwritable files

---
+++ contrib/django/storage.py
@@ -0,0 +1,14 @@
+# Obtained and edited from:
+# https://goo.gl/C3iJyd
+
+from django.core.files.storage import FileSystemStorage
+from django.conf import settings
+import os
+
+
+class OverwriteStorage(FileSystemStorage):
+    def get_available_name(self, name, max_length=None):
+        if self.exists(name):
+            os.remove(os.path.join(settings.MEDIA_ROOT, name))
+        return super(OverwriteStorage, self).get_available_name(
+            name, max_length)
